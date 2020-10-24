2e4b91103093b22cd7cb15a7c005d0a10ffdd191
Stanislav Osipov <lacost.st@gmail.com>
2020-10-24 16:24:01 +0300

    chore: art assets refactor

--- com.stansassets.facebook/Editor/FbEditorWindow.cs
+++ com.stansassets.facebook/Editor/FbEditorWindow.cs
@@ -11,16 +11,19 @@ namespace StansAssets.Facebook.Editor
     class FbEditorWindow : IMGUISettingsWindow<FbEditorWindow>
     {
         public const string WikiHomeUrl = "https://github.com/StansAssets/com.stansassets.facebook/wiki";
-        
+
         static IMGUIDocumentationBlock s_DocumentationBlock;
-       
-        
+
         protected override void OnAwake()
         {
-            titleContent = new GUIContent(FbPackage.DisplayName, (EditorGUIUtility.isProSkin)? EditorAssets.GetImage($"{FbPackageEditor.CoversPath}/logo-dark.png") :  EditorAssets.GetImage($"{FbPackageEditor.CoversPath}/logo-light.png"));
+            titleContent = new GUIContent(FbPackage.DisplayName,
+                EditorGUIUtility.isProSkin
+                    ? EditorAssets.GetImage($"{FbPackageEditor.IconsPath}/logo-dark.png")
+                    : EditorAssets.GetImage($"{FbPackageEditor.IconsPath}/logo-light.png"));
+
             SetPackageName(FbPackage.PackageName);
             SetDocumentationUrl(WikiHomeUrl);
-            
+
             AddMenuItem("Settings", CreateInstance<FbSettingsTab>());
             AddMenuItem("Documentation", CreateInstance<FbDocumentationTab>());
             AddMenuItem("About", CreateInstance<IMGUIAboutTab>());
